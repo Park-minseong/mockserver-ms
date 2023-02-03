0c2bc4b3089342ecafaae155a565c8d700529244
park <qkralstjd02@gmail.com>
2023-02-03 10:46:06 +0900

    AccountBalanceService.java getBalance 에서 throws FileNotFoundException 삭제

--- src/main/java/com/fourvaluesoft/mock/openbanking/account/balance/service/AccountBalanceService.java
+++ src/main/java/com/fourvaluesoft/mock/openbanking/account/balance/service/AccountBalanceService.java
@@ -2,9 +2,7 @@ package com.fourvaluesoft.mock.openbanking.account.balance.service;
 
 import com.fourvaluesoft.mock.openbanking.account.balance.domain.AccountBalance;
 
-import java.io.FileNotFoundException;
-
 public interface AccountBalanceService {
 
-    AccountBalance getBalance(String tranDtime, String rootPath) throws FileNotFoundException;
+    AccountBalance getBalance(String tranDtime, String rootPath);
 }

ce9e86176576886fe100839fed07f40d8d33d9ce
park <qkralstjd02@gmail.com>
2023-02-10 14:02:19 +0900

    AccountBalanceController, AccountRealNameController Servlet관련 Exception제거

--- src/main/java/com/fourvaluesoft/mock/openbanking/account/balance/AccountBalanceController.java
+++ src/main/java/com/fourvaluesoft/mock/openbanking/account/balance/AccountBalanceController.java
@@ -3,19 +3,17 @@ package com.fourvaluesoft.mock.openbanking.account.balance;
 import com.fourvaluesoft.mock.openbanking.account.balance.domain.AccountBalance;
 import com.fourvaluesoft.mock.openbanking.account.balance.service.AccountBalanceService;
 import com.fourvaluesoft.mock.openbanking.account.balance.service.impl.AccountBalanceServiceImpl;
-import com.fourvaluesoft.mock.openbanking.account.exception.AccountNotFoundException;
 import com.fourvaluesoft.mock.openbanking.controller.AccountController;
+import com.fourvaluesoft.mock.openbanking.exception.DataNotFoundException;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class AccountBalanceController extends AccountController {
 
     private final AccountBalanceService accountBalanceService;
 
-    public AccountBalanceController(String webResourcesPath) throws ServletException {
+    public AccountBalanceController(String webResourcesPath) {
         this.method = "GET";
 
         accountBalanceService = new AccountBalanceServiceImpl(webResourcesPath);
@@ -27,7 +25,7 @@ public class AccountBalanceController extends AccountController {
     }
 
     @Override
-    public String processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public String processRequest(HttpServletRequest request, HttpServletResponse response) {
         String tranDtime = getKeyValue(request, "tran_dtime");
 
         try {
@@ -35,7 +33,7 @@ public class AccountBalanceController extends AccountController {
             request.setAttribute("accountBalance", accountBalance);
 
             return getSucceedViewPath();
-        } catch (AccountNotFoundException exception) {
+        } catch (DataNotFoundException exception) {
             request.setAttribute("error", createErrorResponse("A0021", "데이터 없음"));
 
             return getErrorViewPath();
--- src/main/java/com/fourvaluesoft/mock/openbanking/account/realname/AccountRealNameController.java
+++ src/main/java/com/fourvaluesoft/mock/openbanking/account/realname/AccountRealNameController.java
@@ -1,13 +1,12 @@
 package com.fourvaluesoft.mock.openbanking.account.realname;
 
-import com.fourvaluesoft.mock.openbanking.account.exception.AccountNotFoundException;
-import com.fourvaluesoft.mock.openbanking.account.exception.BadRequestException;
 import com.fourvaluesoft.mock.openbanking.account.realname.domain.AccountRealName;
 import com.fourvaluesoft.mock.openbanking.account.realname.service.AccountRealNameService;
 import com.fourvaluesoft.mock.openbanking.account.realname.service.impl.AccountRealNameServiceImpl;
 import com.fourvaluesoft.mock.openbanking.controller.AccountController;
+import com.fourvaluesoft.mock.openbanking.exception.BadRequestException;
+import com.fourvaluesoft.mock.openbanking.exception.DataNotFoundException;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,13 +16,13 @@ public class AccountRealNameController extends AccountController {
 
     private final AccountRealNameService realNameService;
 
-    public AccountRealNameController(String webResourcesPath) throws ServletException {
+    public AccountRealNameController(String webResourcesPath) {
         this.method = "POST";
 
         realNameService = new AccountRealNameServiceImpl(webResourcesPath);
     }
 
-    public String processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public String processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
             String accountNum = getKeyValueFromBody(request, "account_num");
 
@@ -32,12 +31,12 @@ public class AccountRealNameController extends AccountController {
             request.setAttribute("accountRealName", accountRealName);
 
             return getSucceedViewPath();
-        } catch (AccountNotFoundException ex) {
-            request.setAttribute("error", createErrorResponse("A0021", "데이터 없음"));
-
-            return getErrorViewPath();
         } catch (BadRequestException ex) {
             request.setAttribute("error", createErrorResponse("A0003", "요청전문 포맷 에러"));
+
+            return getErrorViewPath();
+        } catch (DataNotFoundException ex) {
+            request.setAttribute("error", createErrorResponse("A0021", "데이터 없음"));
 
             return getErrorViewPath();
         }
